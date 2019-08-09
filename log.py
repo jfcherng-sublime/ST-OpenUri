9d55887abe13d315bf1372f16542b89767a5bd01
Jack Cherng <jfcherng@gmail.com>
2019-08-09 10:43:24 +0800

    Add log.print_msg()
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- log.py
+++ log.py
@@ -10,3 +10,15 @@ def msg(message: str) -> str:
     from .settings import get_package_name
 
     return "[{plugin}] {message}".format(plugin=get_package_name(), message=message)
+
+
+def print_msg(message: str, show_message: bool = True) -> None:
+    """
+    @brief Print plugin message to ST's console.
+
+    @param message      The message
+    @param show_message Whether to print the message
+    """
+
+    if show_message:
+        print(msg(message))

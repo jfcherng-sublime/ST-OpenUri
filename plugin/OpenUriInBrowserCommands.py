91ce587f6d6f9644ac67d628b2b7536625c573bb
Jack Cherng <jfcherng@gmail.com>
2019-08-17 09:00:35 +0800

    Order functions by names (nits)
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/OpenUriInBrowserCommands.py
+++ plugin/OpenUriInBrowserCommands.py
@@ -8,30 +8,6 @@ from .functions import (
 from .settings import get_setting
 
 
-class SelectUriFromViewCommand(sublime_plugin.TextCommand):
-    def run(self, edit: sublime.Edit) -> None:
-        sel = self.view.sel()
-
-        uri_regions = find_uri_regions_by_region(
-            self.view, [0, self.view.size()], get_setting("uri_search_radius")
-        )
-
-        if uri_regions:
-            sel.clear()
-            sel.add_all(uri_regions)
-
-
-class SelectUriFromCursorsCommand(sublime_plugin.TextCommand):
-    def run(self, edit: sublime.Edit) -> None:
-        sel = self.view.sel()
-
-        uri_regions = find_uri_regions_by_regions(self.view, sel, get_setting("uri_search_radius"))
-
-        if uri_regions:
-            sel.clear()
-            sel.add_all(uri_regions)
-
-
 class OpenUriFromCursorsCommand(sublime_plugin.TextCommand):
     def run(self, edit: sublime.Edit, browser="") -> None:
         uris = map(
@@ -56,3 +32,27 @@ class OpenUriFromViewCommand(sublime_plugin.TextCommand):
 
         for uri in set(uris):
             open_uri_from_browser(uri, browser)
+
+
+class SelectUriFromCursorsCommand(sublime_plugin.TextCommand):
+    def run(self, edit: sublime.Edit) -> None:
+        sel = self.view.sel()
+
+        uri_regions = find_uri_regions_by_regions(self.view, sel, get_setting("uri_search_radius"))
+
+        if uri_regions:
+            sel.clear()
+            sel.add_all(uri_regions)
+
+
+class SelectUriFromViewCommand(sublime_plugin.TextCommand):
+    def run(self, edit: sublime.Edit) -> None:
+        sel = self.view.sel()
+
+        uri_regions = find_uri_regions_by_region(
+            self.view, [0, self.view.size()], get_setting("uri_search_radius")
+        )
+
+        if uri_regions:
+            sel.clear()
+            sel.add_all(uri_regions)

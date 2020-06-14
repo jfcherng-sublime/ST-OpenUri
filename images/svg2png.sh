993da8d72ea5f8460b0663ed1135298992beaa31
Jack Cherng <jfcherng@gmail.com>
2020-06-14 16:16:14 +0800

    chore(build): use local installed svg2png
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- images/svg2png.sh
+++ images/svg2png.sh
@@ -9,14 +9,18 @@ SOURCE_DIRS=(
 
 pushd "${SCRIPT_DIR}" || exit
 
+npm install
+
 for source_dir in "${SOURCE_DIRS[@]}"; do
     pushd "${source_dir}" || exit
 
     for svg_file in *.svg; do
+        echo "Converting '${source_dir}/${svg_file}'..."
+
         png_file=${svg_file%.*}.png
 
         rm -f "${png_file}"
-        svg2png "${svg_file}" -w "${PNG_SIZE}" -h "${PNG_SIZE}" -o "${png_file}"
+        npx svg2png "${svg_file}" -w "${PNG_SIZE}" -h "${PNG_SIZE}" -o "${png_file}"
     done
 
     popd || exit

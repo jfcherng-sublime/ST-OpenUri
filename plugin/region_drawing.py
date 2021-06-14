ea0e5de1d6498a4105b16560d20628903a5bc2ec
Jack Cherng <jfcherng@gmail.com>
2021-06-14 14:14:02 +0800

    feat: use strings in setting "draw_uri_regions.flags"
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/region_drawing.py
+++ plugin/region_drawing.py
@@ -1,5 +1,5 @@
 from .settings import get_setting
-from typing import Iterable
+from typing import Iterable, List, Union
 import sublime
 
 
@@ -15,5 +15,13 @@ def draw_uri_regions(view: sublime.View, uri_regions: Iterable[sublime.Region]) 
         list(uri_regions),
         scope=draw_uri_regions["scope"],
         icon=draw_uri_regions["icon"],
-        flags=draw_uri_regions["flags"],
+        flags=parse_draw_region_flags(draw_uri_regions["flags"]),
     )
+
+
+def parse_draw_region_flags(flags: Union[int, List[str]]) -> int:
+    # deprecated because it's not self-explanatory
+    if isinstance(flags, int):
+        return flags
+
+    return sum(map(lambda flag: getattr(sublime, flag, 0), flags))
